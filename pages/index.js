e0da3a458875ac000444a8f1008e511a127b2cea
danielt <hlimajunior@gmail.com>
2020-11-25 15:42:52 -0300

    'Melhorias'

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,36 @@
+
+import { useState } from 'react';
+
+
 function Home() {
     return <div>
-                <h2>LiSi Sistemas Digitais</h2>
-           </div>
+        <h2>LiSi Sistemas Digitais</h2>
+        <CapsLock>Humberto Lima Jr</CapsLock>
+        <CapsLock>Lisi Sistemas</CapsLock>
+        <hr />
+        <Contador />
+    </div>
+}
+
+function CapsLock(props) {
+    const textoInserido = props.children;
+    const textoEmCapsLock = textoInserido.toUpperCase();
+    return <div><font color="red">{textoEmCapsLock}</font></div>
+}
+
+function Contador() {
+
+    const [contador,setContador] = useState(1);
+
+    function adicionarContador(){
+        setContador(contador + 1);
+    }
+    return (
+        <div>
+            <div>{contador}</div>
+            <button onClick={adicionarContador}>Adicionar</button>
+        </div>
+    )
 }
 
 export default Home
