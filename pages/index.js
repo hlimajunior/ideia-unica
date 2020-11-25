5cd65afb2c660c6e4365ca2bd739f5c1c889dddc
danielt <hlimajunior@gmail.com>
2020-11-25 11:49:51 -0300

    'Alteração'

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-    return <div><h2>LiSi Sistemas Digitais</h2></div>
+    return <div>
+                <h2>LiSi Sistemas Digitais</h2>
+           </div>
 }
 
 export default Home
